d73910307bdfdd45d4fc09c68f78d53e2cf6746a
prettyboy <linbin_work@foxmail.com>
2019-12-20 14:28:18 +0800

    update dateutils

--- src/com/candela/workflow/util/DateUtils.java
+++ src/com/candela/workflow/util/DateUtils.java
@@ -3,12 +3,15 @@ package com.candela.workflow.util;
 import com.candela.workflow.bean.Date;
 import com.candela.workflow.bean.Time;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 /**
  * @Author linbin
  * @Create 2019-11-15-16:21
  */
 public class DateUtils {
-    public Date parseDate(String src){
+    public static Date parseDate(String src){
         String[] split = src.split("-");
         Date date = new Date();
         int year = Integer.parseInt(split[0]);
@@ -20,7 +23,7 @@ public class DateUtils {
         date.setDay(day);
         return date;
     }
-    public Time parseTime(String src){
+    public static Time parseTime(String src){
         String[] split = src.split(":");
         Time time = new Time();
         int hour = Integer.parseInt(split[0]);
@@ -32,6 +35,18 @@ public class DateUtils {
         time.setSecond(second);
         return time;
     }
+
+    public static LocalTime  PlusHour(Time time,int hour){
+        LocalTime localTime = LocalTime.of(time.getHour(), time.getMinute(), time.getSecond());
+        LocalTime plusHours = localTime.plusHours(hour);
+        return plusHours;
+    }
+    public static LocalDate PlusDay(Date date, long day){
+        LocalDate localDate = LocalDate.of(date.getYear(),date.getMonth(),date.getDay());
+        LocalDate localDate1 = localDate.plusDays(day);
+        return localDate1;
+    }
+
 
 
 }
